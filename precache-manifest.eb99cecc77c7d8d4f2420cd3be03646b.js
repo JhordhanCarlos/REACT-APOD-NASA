dfe33298571b4900ae793b4cff0065a4514161c5
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2020-01-08 07:54:54 -0300

    Updates

---
+++ precache-manifest.eb99cecc77c7d8d4f2420cd3be03646b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af101c0f8ea108c1823096e732187ca6",
+    "url": "/REACT-APOD-NASA/index.html"
+  },
+  {
+    "revision": "c3355574c64db4ba4746",
+    "url": "/REACT-APOD-NASA/static/css/main.19367fca.chunk.css"
+  },
+  {
+    "revision": "37ba95d7e850dfd44cee",
+    "url": "/REACT-APOD-NASA/static/js/2.98933551.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/REACT-APOD-NASA/static/js/2.98933551.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3355574c64db4ba4746",
+    "url": "/REACT-APOD-NASA/static/js/main.40347054.chunk.js"
+  },
+  {
+    "revision": "e36e8d5a349e4e609517",
+    "url": "/REACT-APOD-NASA/static/js/runtime-main.81727a69.js"
+  }
+]);
